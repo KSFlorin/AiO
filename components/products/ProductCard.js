c3bbd4fec9722b1db5778e1743db3938d0017273
kartop04 <stefan@florin-int.com>
2022-04-10 12:11:25 +0800

    Update ProductCard.js

--- components/products/ProductCard.js
+++ components/products/ProductCard.js
@@ -6,7 +6,7 @@ export default function ProductCard({ permalink, image, name, description, price
     <Link href="/product/[permalink]" as={`/product/${permalink}`}>
       <a className="mb-5 d-block font-color-black cursor-pointer">
         <div
-          className="mb-3"
+          className="mb-3 border rounded border-color-red"
           style={{
             paddingBottom: '125%',
             background: `url("${image}") center center/cover`
@@ -17,8 +17,8 @@ export default function ProductCard({ permalink, image, name, description, price
         <p className="font-size-subheader mb-2 font-weight-medium">
           {name}
         </p>
-        <p className="mb-2 font-color-medium">{description}</p>
-        <p className="font-size-subheader font-weight-medium pb-2 borderbottom border-color-black">
+    
+        <p className="font-size-subheader font-weight-medium pb-2 ">
           {price}
         </p>
       </a>
