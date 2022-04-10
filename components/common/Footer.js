427a5187e98f921876ff0adc3d349bb613bf8a74
kartop04 <stefan@florin-int.com>
2022-04-11 06:12:29 +0800

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import Link from 'next/link'
 
 const Footer = () => (
   <footer className="pt-5">
@@ -7,45 +6,50 @@ const Footer = () => (
       <div className="row">
         <div className="col-12 col-sm-6 col-md-4">
           <p className="font-family-secondary font-size-display1 mb-4">
-        Menu
+            Commerce.js
           </p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
-             
-            <Link href="/">
-          <a>About</a>
-              </Link>
-
-
               <a
-                href="/"
-                className="d-block font-color-medium"
-             
+                href="https://commercejs.com/docs/"
+                className="mb-3 d-block font-color-medium"
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                Our Services
+                Documentation
+              </a>
+              <a
+                href="https://commercejs.com/features"
+                className="d-block font-color-medium"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Features
               </a>
             </div>
             <div>
               <a
-                href="/"
+                href="https://commercejs.com/about"
                 className="mb-3 d-block font-color-medium"
-            
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                How to Order
+                About
               </a>
               <a
-                href="/contact"
+                href="http://slack.commercejs.com/"
                 className="d-block font-color-medium"
-           
+                target="_blank"
+                rel="noopener noreferrer"
               >
-                Contact Us
+                Community
               </a>
             </div>
           </div>
         </div>
         <div className="col-12 col-sm-6 col-md-4">
           <p className="font-family-secondary font-size-display1 mb-4">
-           Social
+            Follow us
           </p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
@@ -55,7 +59,7 @@ const Footer = () => (
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Facebook
+                Twitter
               </a>
               <a
                 href="https://www.instagram.com/commerce.js/"
@@ -63,80 +67,84 @@ const Footer = () => (
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Whatsapp
+                Instagram
               </a>
             </div>
-            
-
-          </div>
-        </div>
-
-
-
-<div className="col-12 col-sm-6 col-md-4">
-          <p className="font-family-secondary font-size-display1 mb-4">
-           Shop Now
-          </p>
-          <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
-            <div className="pr-5">
+            <div>
               <a
-                href="/"
+                href="https://angel.co/company/chec"
                 className="mb-3 d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Facebook store
+                Angel
               </a>
               <a
-                href="/"
+                href="https://www.linkedin.com/company/chec-chec-commerce-inc.-/"
                 className="d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
               >
-                Upload a photo
+                LinkedIn
               </a>
             </div>
-    
           </div>
         </div>
-
-
-
+        <div className="col-12 col-md-4">
+          <p className="font-family-secondary font-size-display1 mb-3">
+            Newsletter
+          </p>
+          <div className="position-relative">
+            <input
+              className="borderbottom border-color-gray400 h-48 w-100 px-3"
+              placeholder="email address"
+            />
+            <button className="bg-transparent position-absolute right-0 top-50 translateY--50 pr-2 h-48">
+              <img src="/icon/arrow-long-right.svg" className="w-24" alt="Arrow icon"/>
+            </button>
+          </div>
+        </div>
       </div>
     </div>
-
     <div className="pt-md-5">
       <div className="bg-brand300">
         <div className="custom-container d-flex flex-column flex-md-row align-items-center justify-content-between">
           <div className="pt-5 pb-0 pt-md-4 pb-md-4 d-flex align-items-center flex-wrap justify-content-center">
-         
-            
             <a
-              href=""
+              href="https://app.netlify.com/start/deploy?repository=https://github.com/chec/commercejs-nextjs-demo-store"
               className="font-color-brand font-size-caption text-uppercase text-center"
               target="_blank"
               rel="noopener noreferrer"
             >
-             Follow us on Facebook
+              Deploy to Netlify
             </a>
             <p className="px-2 font-color-brand font-size-caption">-</p>
             <a
-              href="/terms-of-service"
+              href="https://github.com/chec/commercejs-nextjs-demo-store"
               className="font-color-brand font-size-caption text-uppercase text-center"
               target="_blank"
               rel="noopener noreferrer"
             >
-              Terms of Service
+              Clone in GitHub
+            </a>
+            <p className="px-2 font-color-brand font-size-caption">-</p>
+            <a
+              href="https://github.com/chec/commercejs-nextjs-demo-store"
+              className="font-color-brand font-size-caption text-uppercase text-center"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Contribute
             </a>
           </div>
           <div className="font-color-brand font-size-caption py-4 text-right">
           <a
-              href="/"
+              href="https://commercejs.com/"
               className="font-color-brand font-size-caption text-uppercase text-center"
               target="_blank"
               rel="noopener noreferrer"
             >
-              &copy; { new Date().getFullYear() } Allinone.
+              &copy; { new Date().getFullYear() } Chec/Commerce.js.
             </a>
           </div>
         </div>
