76b32626c7fbb24746ec2d514bc41a61d6b6b4be
kartop04 <stefan@florin-int.com>
2022-04-11 08:42:20 +0800

    Update Footer.js

--- components/common/Footer.js
+++ components/common/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Link from 'next/link'
 
 const Footer = () => (
   <footer className="pt-5">
@@ -9,44 +10,38 @@ const Footer = () => (
         Menu
           </p>
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
+           
             <div className="pr-5">
-              <a
-                href="https://commercejs.com/docs/"
-                className="mb-3 d-block font-color-medium"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                About
-              </a>
-              <a
-                href="https://commercejs.com/features"
-                className="d-block font-color-medium"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Our Services
-              </a>
-            </div>
-            <div>
-              <a
-                href="https://commercejs.com/about"
-                className="mb-3 d-block font-color-medium"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                How to Order
-              </a>
-              <a
-                href="http://slack.commercejs.com/"
-                className="d-block font-color-medium"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Contact Us
-              </a>
+            
+        <Link  href="/">
+          <a className="mb-3 d-block font-color-medium" >Home</a>
+        </Link>
+
+          <Link  href="/about">
+          <a className="mb-3 d-block font-color-medium" >About</a>
+        </Link>
+               
+       
+          <Link  href="/Services">
+          <a className="mb-3 d-block font-color-medium" >Our Services</a>
+        </Link>
+
+          </div>
+
+
+          <div> 
+          <Link  href="/howto">
+          <a className="mb-3 d-block font-color-medium" >How to Order</a>
+          </Link>
+
+           <Link  href="/contact">
+          <a className="mb-3 d-block font-color-medium" >Contact Us</a>
+          </Link>
+
             </div>
           </div>
-        </div>
+          </div>
+
         <div className="col-12 col-sm-6 col-md-4">
           <p className="font-family-secondary font-size-display1 mb-4">
            Social
@@ -54,7 +49,7 @@ const Footer = () => (
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
               <a
-                href="https://twitter.com/commercejs"
+                href="https://Facebook.com/consumergoodsliamnaude"
                 className="mb-3 d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -62,7 +57,7 @@ const Footer = () => (
                 Facebook
               </a>
               <a
-                href="https://www.instagram.com/commerce.js/"
+                href="https://Facebook.com/consumergoodsliamnaude"
                 className="d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -85,7 +80,7 @@ const Footer = () => (
           <div className="d-flex font-color-medium mb-5 pb-3 pb-md-0 mb-md-0">
             <div className="pr-5">
               <a
-                href="https://www.allinone1.hk/"
+                href="https://Facebook.com/consumergoodsliamnaude"
                 className="mb-3 d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -93,7 +88,7 @@ const Footer = () => (
                 Facebook store
               </a>
               <a
-                href="https://www.allinone1.hk/"
+                href="https://tripetto.app/run/3WRVGI02DK"
                 className="d-block font-color-medium"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -117,22 +112,20 @@ const Footer = () => (
          
             
             <a
-              href="https://www.allinone1.hk/"
+              href="https://Facebook.com/consumergoodsliamnaude"
               className="font-color-brand font-size-caption text-uppercase text-center"
               target="_blank"
               rel="noopener noreferrer"
             >
              Follow us on Facebook
             </a>
+
             <p className="px-2 font-color-brand font-size-caption">-</p>
-            <a
-              href="https://www.allinone1.hk/"
-              className="font-color-brand font-size-caption text-uppercase text-center"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Terms of Service
-            </a>
+
+           <Link  href="/terms-of-service">
+          <a className="font-color-brand font-size-caption text-uppercase text-center">Terms of Service</a>
+          </Link>
+
           </div>
           <div className="font-color-brand font-size-caption py-4 text-right">
           <a
